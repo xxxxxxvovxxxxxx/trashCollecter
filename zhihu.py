45ced28f62c4f22cf9e8ae61ce96d00f56efe4aa
xxxxxxvovxxxxxx <59946846+xxxxxxvovxxxxxx@users.noreply.github.com>
2020-05-13 17:59:36 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -1,6 +1,6 @@
-import spiderHelper
+import trashHelper
 
 
 if __name__ == "__main__":
-    res = spiderHelper.getPage('https://www.zhihu.com/question/35215759/answer/1211463956')
+    res = trashHelper.getPage('https://www.zhihu.com/question/35215759/answer/1211463956')
     print(res)
