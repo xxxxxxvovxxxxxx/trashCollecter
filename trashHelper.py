dc109e968421aa553fb5624205f63f16b3d781d5
xxxxxxvovxxxxxx <xxxxxxvovxxxxxx@gmail.com>
2020-05-14 22:36:07 +0800

    optimized helper and add new target

--- trashHelper.py
+++ trashHelper.py
@@ -3,6 +3,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class excelConfig:
+    sheetName = '未命名'
+    path = ''
+    head = []
+    rowHeight = []
+    colWidth = []
+    style = None
+
+    def __init__(self, sheetName, path):
+        self.sheetName = sheetName
+        self.path = path
+    
+    def setStyle(self,style):
+        self.style = style
+
 def getPage(url):
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
@@ -25,28 +40,32 @@ def getInfo(res):
     return list(zip(fTitle, fUrl, rate, payable))
 
 
-def writeExcel(infoList, sheetName, path):
+def writeExcel(infoList, config):
     # need to optimize
-    head = ['电影', '地址', '评分', '状态']
-    if len(infoList) == 0:
+    if infoList is None:
         return "无写入信息"
     row, col = len(infoList), len(infoList[0])
     worksbook = xlwt.Workbook(encoding='utf-8')
-    Worksheet = worksbook.add_sheet(sheetName)
-    Worksheet.col(0).width, Worksheet.col(1).width, Worksheet.col(
-        2).width, Worksheet.col(3).width = 256*20, 256*60, 256*10, 256*10
-    for item in range(col):
-        Worksheet.write(0, item, head[item])
+    Worksheet = worksbook.add_sheet(config.sheetName)
+
+    if len(config.colWidth) != 0:
+        for item in range(col):
+            Worksheet.col(item).width = 256*int(config.colWidth[item])
+    if len(config.head) != 0:
+        for item in range(col):
+            Worksheet.write(0, item, config.head[item])
+    
     try:
         for i in range(0, row):
             for j in range(0, col):
-                if 1 == j:
-                    # to hyperLink
-                    Worksheet.write(
-                        i+1, j, xlwt.Formula('HYPERLINK("{0}")'.format(infoList[i][j])))
-                else:
-                    Worksheet.write(i+1, j, infoList[i][j])
-        worksbook.save(path)
+                # if 1 == j:
+                #     # to hyperLink
+                #     Worksheet.write(
+                #         i+1, j, xlwt.Formula('HYPERLINK("{0}")'.format(infoList[i][j])))
+                # else:
+                #     Worksheet.write(i+1, j, infoList[i][j])
+                Worksheet.write(i+1, j, infoList[i][j],config.style)
+        worksbook.save(config.path)
     except Exception as e:
         return "出现意外："+str(e.args)
     return '完美完成'
@@ -64,11 +83,15 @@ def test():
 
 if __name__ == "__main__":
     infoList = []
+    config = excelConfig('高分电影榜','C:\\Users\\JYX\\Desktop\\挨到看.xls')
+    config.colWidth = [20, 60, 10, 10]
+    config.head = ['电影', '地址', '评分', '状态']
+    print(config)
     for page in range(10):
         url = 'https://movie.douban.com/top250?start={0}&filter='.format(
             page*25)
         html = getPage(url)
         infoList.extend(getInfo(html))
     # print(infoList)
-    res = writeExcel(infoList, '高分电影榜', 'C:\\Users\\JYX\\Desktop\\挨到看.xls')
+    res = writeExcel(infoList, config)
     print(res)
